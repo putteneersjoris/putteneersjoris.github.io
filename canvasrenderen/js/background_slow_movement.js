14997baf7c0f82560eb5d6b8c99706bbc51a2c83
putteneersjoris <putteneersjoris@gmail.com>
2021-09-15 02:13:32 +0200

    big update website

---
+++ canvasrenderen/js/background_slow_movement.js
@@ -0,0 +1,113 @@
+
+var container, stats;
+var camera, scene, controls, renderer;
+var effect;
+
+var frustumSize = 1000;
+
+init();
+animate();
+
+function init(  ) {
+
+	var width = window.innerWidth || 2;
+	var height = window.innerHeight || 2;
+
+	container = document.createElement( 'div' );
+	container.style.position="fixed";
+	document.body.appendChild( container );
+
+	var aspect = window.innerWidth / window.innerHeight;
+	camera = new THREE.OrthographicCamera( frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.001, 2000 );
+	camera.position.y = 200;
+	camera.position.x = 400;
+
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color( "none");
+
+	// Cubes
+	var size_x = Math.floor(Math.random() * 800) + 150;
+	var size_y = Math.floor(Math.random() * 250) + 150;
+	var size_z = Math.floor(Math.random() * 350) + 150;
+	var geometry = new THREE.BoxGeometry(size_x,size_y,size_z);
+	
+	
+	var material = new THREE.MeshLambertMaterial( { color: "white", overdraw: 0 } );
+	var count = Math.floor(Math.random() * 70) + 35;
+	for ( var i = 0; i < count; i ++ ) {
+
+		var cube = new THREE.Mesh( geometry, material );
+		cube.position.x = Math.floor( ( Math.random() * 5000 - 500 ) / 200 ) * 200 + 25;
+		cube.position.y = Math.floor( ( Math.random() * 5000 - 500 ) / 200 ) * 200 + 25;
+		cube.position.y=0;
+		cube.position.z = Math.floor( ( Math.random() * 5000 - 500 ) / 200 ) * 200 + 25;
+
+		scene.add( cube );
+
+	}
+
+	// Lights
+
+	var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
+	scene.add( ambientLight );
+
+	var color1 =3540401.840119951;
+	var directionalLight = new THREE.DirectionalLight( color1 );
+	console.log("color1: "+ color1);
+	directionalLight.position.x = Math.random() - 0.1;
+	directionalLight.position.y = Math.random() - 0.5;
+	directionalLight.position.z = Math.random() - 0.5;
+	directionalLight.position.normalize();
+	scene.add( directionalLight );
+	
+	var color2 =14960462.948172117;
+	var directionalLight = new THREE.DirectionalLight( color2 );
+	console.log("color2: "+ color2);
+	directionalLight.position.x =Math.random() - 0.1;
+	directionalLight.position.y =Math.random() - 0.1;
+	directionalLight.position.z =Math.random() - 0.1;
+	directionalLight.position.normalize();
+	scene.add( directionalLight );
+
+	renderer = new THREE.CanvasRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	//container.appendChild( renderer.domElement );
+	
+	effect = new THREE.AsciiEffect( renderer );
+	effect.setSize( width,height );
+	container.appendChild( effect.domElement );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+	var aspect = window.innerWidth / window.innerHeight;
+	camera.left   = - frustumSize * aspect / 2;
+	camera.right  =   frustumSize * aspect / 2;
+	camera.top    =   frustumSize / 2;
+	camera.bottom = - frustumSize / 2;
+	camera.updateProjectionMatrix();
+	// renderer.setSize( window.innerWidth, window.innerHeight );
+	effect.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+	// controls.update();
+	requestAnimationFrame( animate );
+
+	render();
+}
+
+function render() {
+	var timer = Date.now() * 0.000001;
+	camera.position.x = Math.cos( timer ) * 500;
+	camera.position.z = Math.sin( timer ) * 500;
+	camera.lookAt( scene.position );
+
+	renderer.clear();
+	// renderer.render( scene, camera );
+
+	effect.render( scene, camera );
+
+}
--- canvasrenderen/js/sketch_b.js
+++ canvasrenderen/js/sketch_b.js
@@ -6,29 +6,69 @@ var dx;
 var period = 50.0;
 var amp = 400;
 var screenSize = (Math.random()*700)+600;
+var randValueA =Math.round((Math.random(amp)*3)-1);
+var randValueB =Math.round((Math.random(amp)*10)-1);
+var randValueC =Math.round((Math.random(amp)*2)-1);
+var randValueD =Math.round((Math.random(amp)*2)-1);
+var randValueE =Math.round((Math.random(amp)*2)-1);
+var randValueF =Math.round((Math.random(amp)*2)-1);
+
 
 
 function draw() {
   sinGuy(); 
+  Text();
+  
 }
 
 
 function sinGuy() {
   dx = (PI / period);
 
-  if (h1X >= windowWidth / 1250) {} else {
+  if (h1X >= 1.55) {} else {
     var element;
 
   
     element = createElement("h1",'<a href="https://google.com" target="_blank"><img style="width: 100%; "  src="pic.png"> <a/>');
-
     element.position(-cos(h1X*PI+h1X) * amp+amp, sin(h1X*PI) *amp+amp);
+
+    
 
     h1X += dx;
     h1Y += dx;
   }
-var elementa;
 
-elementa = createElement("div",'<a style="color: green; font-size: 50px;">gegegegegege <a/>');
-elementa.position(1500,500);
+
+
 }
+
+
+function Text() {
+  dx = (PI / period);
+
+  if (h1X >= 6.6) {} else {
+    var  element_a, element_b, element_c;
+
+  
+    
+
+    element_a = createElement("h6",'<a href="#" style="color: blue; font-size: 2em; " >info</a>');
+    element_a.position( 600, h1X  *-200+1300);
+
+    element_b = createElement("h6",'<a href="#" style="color: blue; font-size: 2em; " > lab</a>');
+    element_b.position( 500, h1X  *-300+1300);
+
+    element_c = createElement("h6",'<a href="" style="color: blue; font-size: 2em; " >projects</a>');
+    element_c.position( 20, h1X  *-250+1300);
+
+    
+
+    h1X += dx;
+    h1Y += dx;
+  }
+
+  element_c = createElement("h6",'<a href="alien.html" style="color: blue; text-decoration: none; opacity: 0.1; font-size: 1em; " >&#128125</a>');
+  element_c.position( sin(h1X*PI)*15+500, cos(h1Y*PI)*15+1000);
+
+}
+
